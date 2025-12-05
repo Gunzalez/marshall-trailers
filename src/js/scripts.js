5e5886d2e6cb5d56f3902c963650ef16dba4c3c4
Segun Konibire <gunzalez@hotmail.com>
2025-12-05 13:39:39 +0000

    implements Vue based mini carousel

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -97,7 +97,7 @@
      * ### object attributes
      * utils for adding and extracting data attributes from HTMl elements
      *
-     * @returns object containing swetter and getter functions
+     * @returns object containing setter and getter functions
      */
     attributes: {
       set: function (element, attr, value) {
@@ -182,6 +182,30 @@
           }
         });
       });
+    },
+  };
+
+  marshallTrailers.miniCarousel = {
+    $carouselMini: null,
+
+    init: function () {
+      this.$carouselMini = $(".carousel-mini").slick({
+        dots: false,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        infinite: true,
+        arrows: false,
+      });
+    },
+
+    next: function () {
+      this.$carouselMini.slick("slickNext");
+    },
+
+    prev: function () {
+      this.$carouselMini.slick("slickPrev");
     },
   };
 
@@ -318,6 +342,8 @@
     marshallTrailers.topNavigation.init();
     marshallTrailers.mobileNavigation.init();
     marshallTrailers.carousel.init();
+
+    window.miniCarousel = marshallTrailers.miniCarousel;
   };
 
   /** Runs the global init */
