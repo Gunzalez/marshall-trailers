2434e0ec3be34960dcba98a04803f4251a7049f5
Segun Konibire <gunzalez@hotmail.com>
2026-01-30 23:12:31 +0000

    generated changes

--- website/js/scripts.js
+++ website/js/scripts.js
@@ -916,6 +916,7 @@ window.data = data;
     carousel: null,
     totalSlides: 7,
     visibleSlides: 3,
+
     next: function () {
       this.carousel.slick("slickNext");
     },
@@ -1018,12 +1019,27 @@ window.data = data;
 
   marshallTrailers.basket = {
     $element: null,
+
     open: function () {
       this.$element.addClass("open");
     },
+
     close: function () {
       this.$element.removeClass("open");
     },
+
+    setToBusy: function () {
+      this.$element.addClass("busy");
+    },
+
+    update: function (numItems, totalPrice) {
+      var strS = numItems > 1 ? "s" : "";
+      this.$element.find("#num_items").text(numItems + " item" + strS);
+      this.$element.find("#total_price").text(totalPrice);
+      this.$element.addClass("open");
+      this.$element.removeClass("busy");
+    },
+
     init: function () {
       this.$element = $("#floating-basket");
       this.$element.find(".desktop-button").on("click", function () {
@@ -1056,8 +1072,8 @@ window.data = data;
         touchswipe: false,
         mousewheel: false,
         txt_toggle_cmd: false,
-        max_width: "60%",
-        max_height: "63%",
+        max_width: "65%",
+        max_height: "65%",
       });
     },
 
@@ -1067,8 +1083,8 @@ window.data = data;
         gallery: false,
         skin: "dark",
         txt_toggle_cmd: false,
-        max_width: "60%",
-        max_height: "63%",
+        max_width: "65%",
+        max_height: "65%",
       });
     },
 
