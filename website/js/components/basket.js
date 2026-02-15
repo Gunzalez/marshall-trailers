60fc6c810a14510b071d66df8a05cb97e2319788
Segun Konibire <gunzalez@hotmail.com>
2026-02-15 15:20:17 +0000

    refactor basket component to use props for options and selection logic

--- website/js/components/basket.js
+++ website/js/components/basket.js
@@ -2,16 +2,30 @@ const { computed, onMounted } = Vue;
 
 export default {
   props: {
-    selection: Array,
+    options: Array,
+    initialOption: Object, // The initial machine
   },
 
-  setup() {
+  setup(props) {
+    const selection = computed(() => {
+      if (props.initialOption) {
+        return [
+          props.initialOption,
+          ...props.options.filter((option) => option.isSelected),
+        ];
+      }
+      return props.options.filter((option) => option.isSelected);
+    });
+
     onMounted(() => {
       console.log("Basket mounted");
     });
+    return {
+      selection,
+    };
   },
   template: `
-    <div v-if="selection.length" class="options-basket">
+    <div v-if="options.length" class="options-basket">
         <div class="heading-wrapper">
           <div class="heading">
               <div class="action-step">Step 03.</div>
