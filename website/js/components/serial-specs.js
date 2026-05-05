c9a2d5c67e1f11c9b117698de90da3788b65fe35
Segun Konibire <gunzalez@hotmail.com>
2026-05-05 13:23:01 +0100

    improves specifications display on serial number screen

--- website/js/components/serial-specs.js
+++ website/js/components/serial-specs.js
@@ -12,14 +12,19 @@ export default {
   },
   template: `
     <div class="serial-specs">
-      <table class="specs-table">
-          <tbody>
-            <tr v-for="(spec, index) in props.specification" :key="index">
-              <th valign="top"><span class="label">{{ spec.name }}</span></th>
-              <td valign="top"><span class="value">{{ spec.value }}</span></td>
-            </tr>
-          </tbody>
-      </table>
+      <div class="serial-specs-wrapper">
+        <div class="specs-table-wrapper">
+            <table class="specs-table">
+              <tbody>
+                <tr v-for="(spec, index) in props.specification" :key="index">
+                  <th valign="top"><span class="label">{{ spec.name }}</span></th>
+                  <td valign="top"><span class="value">{{ spec.value }}</span></td>
+                </tr>
+              </tbody>
+            </table>
+        </div>
+        <div class="scroll-indicator"></div>
+      </div>
       <div class="specs-actions">
         <a href="#" @click.prevent="$emit('more-details-click')" class="bttn btn_ToggleOptions Chevron-Right">More Details</a>
         <a href="#" @click.prevent="$emit('not-my-marshall-click')" class="body-link">Not Your Marshall?</a>
